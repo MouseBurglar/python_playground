1464514f2325758068a50f0c89dd70c958c8db5b
mouseburglar <daniel.singer.ch@gmail.com>
2016-04-18 11:17:31 +0200

    Not yet properly working. drawMatches() only exists in OpenCV 3.0.1. Now with community version with a bug ...

--- main.py
+++ main.py
@@ -1,56 +1,87 @@
 import numpy as np
 import cv2
+import scipy as sp
 import matplotlib as plt
 
 
-"""
-hessian = 400
-img = cv2.imread('pp.jpg', 1)
-surf = cv2.SURF(hessian)
-kp, des = surf.detectAndCompute(img, None)
+def drawMatches(img1, kp1, img2, kp2, matches):
+    """
+    My own implementation of cv2.drawMatches as OpenCV 2.4.9
+    does not have this function available but it's supported in
+    OpenCV 3.0.0
 
-print len(kp)
+    This function takes in two images with their associated
+    keypoints, as well as a list of DMatch data structure (matches)
+    that contains which keypoints matched in which images.
 
-img2 = cv2.drawKeypoints(img, kp, None, (0, 255, 0), 4)
-cv2.namedWindow('Guatemala', cv.WINDOW_NORMAL)
-cv2.imshow('Guatemala', img2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
+    An image will be produced where a montage is shown with
+    the first image followed by the second image beside it.
 
-"""
-cv.namedWindow('Guatemala', cv.WINDOW_NORMAL)
-cv.imshow('Guatemala', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
-"""
+    Keypoints are delineated with circles, while lines are connected
+    between matching keypoints.
 
-img1 = cv2.imread('template.jpg', 0)          # queryImage
-img2 = cv2.imread('watch.jpg', 0) # trainImage
-hessian = 400
+    img1,img2 - Grayscale images
+    kp1,kp2 - Detected list of keypoints through any of the OpenCV keypoint
+              detection algorithms
+    matches - A list of matches of corresponding keypoints through any
+              OpenCV keypoint matching algorithm
+    """
 
-# Initiate SIFT detector
-sift = cv2.SIFT()
+    # Create a new output image that concatenates the two images together
+    # (a.k.a) a montage
+    rows1 = img1.shape[0]
+    cols1 = img1.shape[1]
+    rows2 = img2.shape[0]
+    cols2 = img2.shape[1]
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1, None)
-kp2, des2 = sift.detectAndCompute(img2, None)
+    out = np.zeros((max([rows1, rows2]), cols1+cols2, 3), dtype='uint8')
 
-# BFMatcher with default params
+    # Place the first image to the left
+    out[:rows1, :cols1, :] = np.dstack([img1, img1, img1])
+
+    # Place the next image to the right of it
+    out[:rows2, cols1:cols1+cols2, :] = np.dstack([img2, img2, img2])
+
+    # For each pair of points we have between both images
+    # draw circles, then connect a line between them
+    for mat in matches:
+
+        # Get the matching keypoints for each of the images
+        img1_idx = mat.queryIdx
+        img2_idx = mat.trainIdx
+
+        # x - columns
+        # y - rows
+        (x1, y1) = kp1[img1_idx].pt
+        (x2, y2) = kp2[img2_idx].pt
+
+        # Draw a small circle at both co-ordinates
+        # radius 4
+        # colour blue
+        # thickness = 1
+        cv2.circle(out, (int(x1), int(y1)), 4, (255, 0, 0), 1)
+        cv2.circle(out, (int(x2)+cols1, int(y2)), 4, (255, 0, 0), 1)
+
+        # Draw a line in between the two points
+        # thickness = 1
+        # colour blue
+        cv2.line(out, (int(x1), int(y1)), (int(x2)+cols1, int(y2)), (255, 0, 0), 1)
+
+    # Show the image
+    cv2.imshow('Matched Features', out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+img1 = cv2.imread('template.jpg', cv2.COLOR_RGB2GRAY)
+img2 = cv2.imread('watch.jpg', cv2.COLOR_RGB2GRAY)
+
+surf = cv2.ORB()
+
+kp1, des1 = surf.detectAndCompute(img1, None)
+kp2, des2 = surf.detectAndCompute(img2, None)
+
 bf = cv2.BFMatcher()
-matches = bf.knnMatch(des1, des2, k=2)
+matches = bf.match(des1, des2)
+matches = sorted(matches, key=lambda x: x.distance)
 
-# Apply ratio test
-good = []
-for m, n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
-
-print len(good)
-
-# cv2.drawMatchesKnn expects list of lists as matches.
-img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, flags=2)
-
-cv2.imshow('matches', img3)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+drawMatches(img1, kp1, img2, kp2, matches[:10])
