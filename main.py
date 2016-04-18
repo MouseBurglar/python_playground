e85b6e95d65b958d49dd0153d200543ac050f5eb
mouseburglar <daniel.singer.ch@gmail.com>
2016-04-18 10:19:52 +0200

    First time in D13

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-def function(a):
-    print a
+import cv2 as cv
+import matplotlib as plt
 
-function("Hello World!")
-function("Alright!")
+
+hessian = 8000
+img = cv.imread('messi.jpg', 1)
+surf = cv.SURF(hessian)
+kp, des = surf.detectAndCompute(img, None)
+
+print len(kp)
+
+img2 = cv.drawKeypoints(img, kp, None, (0, 255, 0), 4)
+cv.imshow('CV', img2)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+"""
+cv.namedWindow('Guatemala', cv.WINDOW_NORMAL)
+cv.imshow('Guatemala', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+"""
